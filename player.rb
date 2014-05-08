2708bdf349f5871600fb11640fb1a48185f16978
Damon Siefert <siefert@gmail.com>
2014-05-08 02:41:07 -0400

    event on leaving a level

--- player.rb
+++ player.rb
@@ -85,12 +85,14 @@ module Roguelike
 		end
 
 		def descend
+			Event.new(:leave, self)
 			if Event.new(:descend, self, local: [x, y]).unheard?
 				Dispatcher.queue_message("You attempt to descend but there is nothing to descend.")
 			end
 		end
 
 		def ascend
+			Event.new(:leave, self)
 			if Event.new(:ascend, self, local: [x, y]).unheard?
 				Dispatcher.queue_message("What do you expect to climb, exactly?")
 			end
