44fb7c013e191f0e0316a6e25bd4ea1bca496dae
Damon Siefert <siefert@gmail.com>
2014-06-02 00:30:54 -0400

    fortunes

--- fortunes.rb
+++ fortunes.rb
@@ -6,6 +6,9 @@ module Roguelike
 		"Beware of Canadians.",
 		"Stairs don't exist and have never existed.",
 		"Death is the only thing you find easily in the dungeons.",
-		"Don't bother looking for the Amulet of Yendor."
+		"Don't bother looking for the Amulet of Yendor.",
+		"Don't be intimidated. It's only magic.",
+		"I've heard that Elves are into nudism.",
+		"A goblin told me there's a wand of infinite wishes just down the stairs."
 	]
 end
