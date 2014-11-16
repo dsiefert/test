b4f8e4661961bfd82c943dc5db26c1779f35a028
Damon Siefert <damon@ornicept.com>
2014-11-15 20:25:13 -0500

    allow items to be taken into the inventory

--- items/item.rb
+++ items/item.rb
@@ -7,18 +7,30 @@ module Roguelike
 		class Item < ::Roguelike::Point
 			include ::Roguelike::Event::Capable
 
-			attr_reader   :name
+			attr_reader   :name, :takeable, :fancy_name
 			attr_accessor :color
 
 			def initialize(x, y, name, character, color, params = {}) #will later add type or something
 				super(x, y)
 
 				@name, @character, @color = name, character, color
+				@takeable                 = !!params[:takeable]
+				@fancy_name               = params[:fancy_name] || name.capitalize
 
 				listen_for(:tread, Game.player) { Dispatcher.queue_message("You step on a #{name}!") }
 
 				listen_for(:hug, Game.player) do |me|
 					Dispatcher.queue_message("You hug the #{name}. What kind of freak are you?")
+				end
+
+				listen_for(:take, Game.player) do |me|
+					if @takeable
+						Dispatcher.queue_message("You take the #{name}.")
+						Game.player.inventory.add(me)
+						Game.level.remove_movable(self)
+					else
+						Dispatcher.queue_message("You can't possibly take the #{name}!")
+					end
 				end
 
 				@walkable = true
